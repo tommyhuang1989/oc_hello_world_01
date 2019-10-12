5f73e3e5ea6d45e6077d725cabf58088dcc78e58
easeware <easeware@easewaredeMac.local>
2019-10-12 19:41:11 +0800

    add hello world to github

---
+++ oc_hello_world_01/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  oc_hello_world_01
+//
+//  Created by easeware on 2019/10/11.
+//  Copyright © 2019年 bugfine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+
+//#include <stdio.h> // oc 的导入头文件使用 #import <Foundation/Foundation.h>
+//
+//int main() {
+//    printf("hello world.\r\n"); //oc 新增了输出方法 NSLog()
+//    return 0;
+//}
+
